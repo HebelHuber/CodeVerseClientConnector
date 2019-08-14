c755b9b0b93e28a43dab5d8ab6d8e1dfbfee9aec
Reeley <sourze@web.de>
2019-08-14 22:49:56 +0200

    Networking

--- CodeVerse.ClientConnector.Tester/Program.cs
+++ CodeVerse.ClientConnector.Tester/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 using CodeVerse.ClientConnector;
 
 namespace CodeVerse.ClientConnector.Tester
@@ -7,7 +8,15 @@ namespace CodeVerse.ClientConnector.Tester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpClient client = new TcpClient("localhost",1337);
+
+            for (int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                byte[] _msg = System.Text.Encoding.UTF8.GetBytes("HELLO WORLD!");
+                client.GetStream().Write(_msg, 0, _msg.Length);
+            }
+            client.Close();
         }
     }
 }
